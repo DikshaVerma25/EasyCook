8737f97d66c3a904e9fd57b6f83f68c88ffe0e4e
DikshaVerma25 <dikshaverma235@gmail.com>
2023-03-02 23:21:39 -0500

    m

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,10 @@
-##############
+import dotenv from 'dotenv';
+import express from 'express';
+
+const app = express();
+const port = '4000';
+
+dotenv.config();
+const PORT = process.env.PORT || port;
+
+
