d1ac3b22c442c47c5518d57fb2e9c3943d32dae9
DikshaVerma25 <dikshaverma235@gmail.com>
2023-03-23 15:26:40 -0400

    interface and schema created

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, {Document, Schema} from "mongoose";
+import bcrypt from 'bcryptjs'; 
 
 export interface User extends Document{
     FirstName: string;
@@ -10,9 +11,24 @@ export interface User extends Document{
 const userSchema: Schema = new mongoose.Schema({
     Firstname: {type: String, required:true},
     LastName: {type: String, required:true},
-    email: {type: String, required:true},
+    email: {type: String, required:true, unique: true},
     password: {type: String, required:true}
 
+},{ timestamps: true }
+);
+
+userSchema.pre<User>('save', async function (next) {
+    const user = this; 
+    if(!user.isModified('password'))
+    {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password , salt);
+
+    user.password = hash;
+    return next();
+    
 });
 
 export default mongoose.model<User>("User", userSchema)
