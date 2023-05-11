946fe97ec2f491deb7a0e60828dd02d0dab06580
DikshaVerma25 <dikshaverma235@gmail.com>
2023-05-11 09:02:32 -0400

    frontend start

--- server/src/app.js
+++ server/src/app.js
@@ -7,11 +7,20 @@ const userRoutes = require('./routes /userRoutes.js');
 
 dotenv.config();
 
+// Set the MONGODB_URI environment variable
+process.env.MONGODB_URI = 'mongodb://localhost:27017/EasyCook';
+
+// Check if the variable is defined
+console.log(process.env.MONGODB_URI);
+
+
 console.log("before env")
 console.log(process.env.MONGODB_URI);
 console.log("check");
 
 const app = express();
+
+app.use(morgan('tiny'));
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -19,7 +28,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
+//   findOneAndUpdate: true, // Enable findOneAndUpdate()
+//   updateOne: true, // Enable updateOne()
+//   updateMany: true, // Enable updateMany()
+//   indexes: true
 })
   .then(() => console.log('MongoDB connected'))
   .catch(error => console.error(error));
