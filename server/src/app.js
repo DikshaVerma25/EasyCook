4dccd4f5e315844761b6efc01dbc9a089929b221
DikshaVerma25 <dikshaverma235@gmail.com>
2023-05-30 09:52:33 -0400

    restart

--- server/src/app.js
+++ server/src/app.js
@@ -1,40 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const morgan = require('morgan');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/ authRoutes');
-const userRoutes = require('./routes/userRoutes.js');
+const authRoutes = require('./routes /authRoutes');
 
-dotenv.config();
+//const userRoutes = require('./routes /userRoutes.js');
+// import authRoutes from "./routes/authRoutes"
 
-// Set the MONGODB_URI environment variable
-// process.env.MONGODB_URI = 'mongodb://localhost:27017/EasyCook';
-
-// Check if the variable is defined
-console.log(process.env.MONGODB_URI);
-
-
-console.log("before env")
-console.log(process.env.MONGODB_URI);
-console.log("check");
+require("dotenv").config();
 
 const app = express();
-
-app.use(morgan('tiny'));
-
-app.use(morgan('dev'));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-//   findOneAndUpdate: true, // Enable findOneAndUpdate()
-//   updateOne: true, // Enable updateOne()
-//   updateMany: true, // Enable updateMany()
-//   indexes: true
 })
   .then(() => console.log('MongoDB connected'))
-  .catch(error => console.error(error));
+  .catch(error => console.error(error, "Error conecting to database"));
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
--- server/src/routes /authRoutes.js
+++ server/src/routes /authRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const authController = require('../controllers/authControllers');
+const userController = require('../controllers/userControllers');
+const middleware = require('../utils/middleware');
 
 const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+
+
+router.get('/me', middleware.authenticate, userController.getCurrentUser);
 
 module.exports = router;
