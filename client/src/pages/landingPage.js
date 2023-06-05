0762bbcdf96178377a597ed41bf52a7510c2c865
DikshaVerma25 <dikshaverma235@gmail.com>
2023-06-05 15:39:28 -0400

    images

--- client/src/pages/landingPage.js
+++ client/src/pages/landingPage.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/navbar';
 import "./pages.css";
-import image from "../images/wonton.jpeg"
+
 
 function LandingPage() {
+  const imageFolder = "../images/bg/";
+  const imageNames = ["bg-1.avif","bg-2.avif","bg-3.avif","bg-4.jpeg","bg-5.avif","bg-6.avif","bg-7.avif","bg-8.avif","bg-9.avif","bg-10.avif"]; // Array of image names
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageNames.length);
+    }, 5000); 
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [imageNames.length]);
+
+
+  const getImageSource = () => {
+    const currentImageName = imageNames[currentImageIndex];
+    return `${imageFolder}${currentImageName}`;
+  };
+
+  // console.log(image);
   return (
     <div>
       <Navbar />
       <div className="landing-container">
-        <h1>Your Indregients our recipies</h1>
+        <h1>Your Ingredients our recipes</h1>
         <div className="left-image-container">
-        <img src={image} alt="Image" className="left-image" />
+          <img src={getImageSource()} alt="Image" className="left-image" />
         </div>
         <div className="symbol-container">
           <div className="symbol turnip"></div>
@@ -23,5 +44,3 @@ function LandingPage() {
 }
 
 export default LandingPage;
-
-
