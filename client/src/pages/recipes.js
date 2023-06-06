ef73ffcdb70782ea5ffd0ca36da9e6b7938bbb92
DikshaVerma25 <dikshaverma235@gmail.com>
2023-06-06 15:04:39 -0400

    recipe card added

--- client/src/pages/recipes.js
+++ client/src/pages/recipes.js
@@ -3,12 +3,34 @@ import axios from 'axios';
 import Navbar from '../components/navbar';
 import './styles/recipes.css';
 
-const RecipesPage = () => {
+function RecipesPage() {
   const [ingredients, setIngredients] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [recipes, setRecipes] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+
+  useEffect(() => {
+    // Make API request to retrieve recipes based on ingredients
+    const getRecipes = async () => {
+      try {
+        const response = await axios.get('https://api.spoonacular.com/recipes/findByIngredients', {
+          params: {
+            ingredients: ingredients.join(','),
+            number: 8,
+            apiKey: 'f5098cf6c3b14c549c3ad15a79fffe14',
+            ignorePantry: false,
+          },
+        });
+
+        setRecipes(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (ingredients.length > 0) {
+      getRecipes();
+    }
+  }, [ingredients]);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -21,50 +43,27 @@ const RecipesPage = () => {
     }
   };
 
-  const handlePreviousPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
-  };
-
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
-
-  useEffect(() => {
-    const fetchRecipes = async () => {
-        try {
-          const response = await axios.get(
-            `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients.join(',')}&apiKey=YOUR_API_KEY`
-          );
-          setRecipes(response.data);
-        } catch (error) {
-          console.error('Error fetching recipes:', error);
-        }
-      };
-      
-
-    fetchRecipes();
-  }, [ingredients, currentPage]);
-
   return (
     <div>
       <Navbar />
       <div className="recipes-page">
         <div className="search-box">
-            <div className="input-section">
-          <input
-            type="text"
-            placeholder="Add ingredient"
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-          <button onClick={handleAddIngredient}>Add</button>
-        </div>
-        <div className="ingredient-list">
-          {ingredients.map((ingredient, index) => (
-            <div key={index} className="ingredient">
-              {ingredient}
-            </div>
-          ))}
+          <div className="input-section">
+            <input
+              type="text"
+              placeholder="Add ingredient"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+            <button onClick={handleAddIngredient}>Add</button>
+          </div>
+          <div className="ingredient-list">
+            {ingredients.map((ingredient, index) => (
+              <div key={index} className="ingredient">
+                {ingredient}
+              </div>
+            ))}
+          </div>
         </div>
         <div className="recipe-cards">
           {recipes.map((recipe) => (
@@ -74,14 +73,9 @@ const RecipesPage = () => {
             </div>
           ))}
         </div>
-        <div className="pagination">
-          {currentPage > 1 && <button onClick={handlePreviousPage}>Previous</button>}
-          {currentPage < totalPages && <button onClick={handleNextPage}>Next</button>}
-        </div>
-      </div>
       </div>
     </div>
   );
-};
+}
 
 export default RecipesPage;
