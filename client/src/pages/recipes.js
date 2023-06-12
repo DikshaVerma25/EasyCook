74aaed6d9bc73a15aee7b37bfefc9c10312f8acf
DikshaVerma25 <dikshaverma235@gmail.com>
2023-06-12 15:45:10 -0400

    n

--- client/src/pages/recipes.js
+++ client/src/pages/recipes.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../components/navbar';
 import './styles/recipes.css';
-import "@fortawesome/fontawesome-svg-core"
+// import nltk from nltk.corpus import wordnet
+
+nltk.download('averaged_perceptron_tagger')
 
 function RecipesPage() {
   const [ingredients, setIngredients] = useState([]);
@@ -13,23 +15,18 @@ function RecipesPage() {
   const [recipesPerPage] = useState(8);
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [ingredientToRemove, setIngredientToRemove] = useState('');
-  const [filter, setFilter] = useState('');
-  const navigate = useNavigate();
 
   useEffect(() => {
-    
+    // Make API request to retrieve recipes based on ingredients
     const getRecipes = async () => {
       try {
-        const response = await axios.get(
-          'https://api.spoonacular.com/recipes/findByIngredients',
-          {
-            params: {
-              ingredients: ingredients.join(','),
-              apiKey: 'f5098cf6c3b14c549c3ad15a79fffe14', // Replace with your Spoonacular API key
-              ignorePantry: false,
-            },
-          }
-        );
+        const response = await axios.get('https://api.spoonacular.com/recipes/findByIngredients', {
+          params: {
+            ingredients: ingredients.join(','),
+            apiKey: 'f5098cf6c3b14c549c3ad15a79fffe14',
+            ignorePantry: false,
+          },
+        });
 
         setRecipes(response.data);
       } catch (error) {
@@ -74,26 +71,13 @@ function RecipesPage() {
     setShowConfirmation(false);
   };
 
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value);
-  };
-
-
-  const filteredRecipes = filter
-    ? recipes.filter((recipe) => recipe.title.toLowerCase().includes(filter.toLowerCase()))
-    : recipes;
-
- 
+  // Get current recipes
   const indexOfLastRecipe = currentPage * recipesPerPage;
   const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
-  const currentRecipes = filteredRecipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
 
-  
-  const totalPages = Math.ceil(filteredRecipes.length / recipesPerPage);
-
-  const handleRecipeClick = (recipeId) => {
-    navigate(`/recipe/${recipeId}`);
-  };
+  // Calculate total number of pages
+  const totalPages = Math.ceil(recipes.length / recipesPerPage);
 
   return (
     <div>
@@ -120,24 +104,15 @@ function RecipesPage() {
             ))}
           </div>
         </div>
-        <div className="filter-section">
-          <input
-            type="text"
-            placeholder="Filter recipes"
-            value={filter}
-            onChange={handleFilterChange}
-          />
-        </div>
         {ingredients.length === 0 ? (
           <div className="no-ingredients-message">
-            Ready, set, spice it up! Ingredients? Not yet added! But fear not, flavor awaits.
-            Stay tuned as we whip up a recipe search feast that'll leave you craving for more!
+            Ready, set, spice it up! Ingredients? Not yet added! But fear not, flavor awaits. Stay tuned as we whip up a recipe search feast that'll leave you craving for more!
           </div>
         ) : (
           <div className="recipe-cards">
             {currentRecipes.map((recipe) => (
               <Link key={recipe.id} to={`/recipe/${recipe.id}`} className="recipe-card">
-                <img src={`https://spoonacular.com/recipeImages/${recipe.image}`} alt={recipe.title} />
+                <img src={recipe.image} alt={recipe.title} />
                 <h3>{recipe.title}</h3>
               </Link>
             ))}
