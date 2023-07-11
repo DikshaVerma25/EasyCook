2d389529355cc8dc5e81770e56803dffad9dc3ec
DikshaVerma25 <dikshaverma235@gmail.com>
2023-07-11 16:29:41 -0400

    sign-in buttons added

--- client/src/pages/signin.js
+++ client/src/pages/signin.js
@@ -9,17 +9,31 @@ function SignIn({ closeSignInModal }) {
   };
 
   return (
+    
     <div className="sign-in-modal">
         <div className="left-image-container">
           <img src={Image} alt="Image" className="left-image" />
         </div>
         <div className="right-content">
         <h1 id= "modal-title">Discover EasyCook</h1>
-        <h3 id="description">Where Delicious Recipes Meet Sustainability</h3>
+        <h5 id="description">Where Delicious Recipes Meet Sustainability</h5>
         <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button onClick={handleSignIn}>Sign In</button>
+          <div className="form-row">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" />
+          </div>
+          <div className="form-row">
+            <input type="email" placeholder="Email" />
+          </div>
+          <div className="form-row">
+            <div className="password-row">
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder="Re-enter Password" />
+            </div>
+            </div>
+          <div className="form-row">
+            <button className="create-account-button">Create Account</button>
+          </div>
         </form>
         </div>
     </div>
